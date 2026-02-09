4ec3676b7ddd33c9ca590f4e12069b71ae4fd434
arowojolu17-spec <arowojolu17@gmail.com>
2026-02-09 15:36:58 +0100

    Update BuyBPCPayment.tsx

--- src/pages/BuyBPCPayment.tsx
+++ src/pages/BuyBPCPayment.tsx
@@ -116,12 +116,12 @@ const BuyBPCPayment = () => {
         <div className="mb-3 border-t pt-3">
           <p className="text-gray-500 text-xs">Account Number</p>
           <div className="flex justify-between items-center">
-            <p className="text-lg font-bold">8110221728</p>
+            <p className="text-lg font-bold">8051000614</p>
             <Button
               variant="default"
               size="sm"
               className="bg-blue-600 hover:bg-blue-700 flex items-center gap-1"
-              onClick={() => handleCopy("8110221728", "Account Number")}
+              onClick={() => handleCopy("8051000614", "Account Number")}
             >
               <Copy size={14} />
               Copy
@@ -136,7 +136,7 @@ const BuyBPCPayment = () => {
 
         <div className="mb-3 border-t pt-3">
           <p className="text-gray-500 text-xs">Account Name</p>
-          <p className="text-lg font-bold">AROWOJOLU  ABIDEMI</p>
+          <p className="text-lg font-bold">OLADETAN OLUWASEUN</p>
         </div>
       </div>
 
